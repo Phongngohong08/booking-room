d45d188aad61e33c48de99c7dc79f7bfd6c33379
Phongngohong08 <CT070337@actvn.edu.vn>
2025-04-17 19:08:22 +0700

    fix password encoder

--- src/main/java/com/phongngohong08/bookingroom/services/impl/UserServiceImpl.java
+++ src/main/java/com/phongngohong08/bookingroom/services/impl/UserServiceImpl.java
@@ -17,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -28,13 +29,13 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserServiceImpl implements UserService {
 
-    PasswordEncoder passwordEncoder;
     UserRepository userRepository;
     RoleRepository roleRepository;
     UserMapper userMapper;
 
     public UserResponse createUser(RegisterRequest request) {
 
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
         User user = userMapper.toUser(request);
         user.setHashedPassword(passwordEncoder.encode(request.getPassword()));
 
